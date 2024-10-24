71f7d5e47a3666b1896f5b4fb1f9bc7f7a8bd6e2
AliKhachab <a.khachab620@gmail.com>
2024-10-24 00:17:48 -0400

    updates to submit form

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-// COUNTER COMPONENT
+// GLOBAL
 const textareaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
+const formEl = document.querySelector('.form');
+
+// COUNTER COMPONENT
 
 const textareaInputListener = () => { 
     const maxChars = textareaEl.maxLength;
@@ -13,11 +16,55 @@ const textareaInputListener = () => {
 textareaEl.addEventListener('input', textareaInputListener);
 
 // SUBMIT COMPONENT
-const formEl = document.querySelector('.form');
 
-const submitHandler = (event) => {
-    // prevent default browser action
+const submitHandler = event => {
+    // prevent default browser action of refreshing page on submit press
     event.preventDefault();
+
+    // get text from text area and validate it
+    const text = textareaEl.value;
+    if (text.includes('#') && text.length >= 5) {
+        formEl.classList.add('form--valid');
+        setTimeout(() => {
+            formEl.classList.remove('form--valid');
+        }, 2000);
+    } else {
+        formEl.classList.add('form--invalid');
+        setTimeout(() => {
+            formEl.classList.remove('form--invalid');
+        }, 2000);
+        
+        textareaEl.focus();
+
+        return;
+    }
+
+    // get the first company name hashtag in the text (if charAt index 0 of a word is #, that is now the hashtag)
+    const hashtag = text.split(' ').find(element => element.charAt(0) === '#');
+    const companyName = hashtag.substring(1);
+    const badgeLetter = companyName.charAt(0).toUpperCase();
+    const upvoteCount = 0;
+    const daysAgo = 0;
+
+    // new feedback item HTML
+
+    const feedbackItemHTML = 
+    `<li class="feedback">
+        <button class="upvote">
+            <i class="fa-solid fa-caret-up upcvote__icon"></i>
+            <span class="upvote__count">${upvoteCount}</span>
+        </button>
+        <section class="feedback__badge">
+            <p class="feedback__letter">${badgeLetter}</p>
+        </section>
+        <div class="feedback__content">
+            <p class="feedback__company">${companyName}</p>
+            <p class="feedback__text">${text}</p>
+        </div>
+        <p class="feedback__date">${daysAgo}</p>
+    </li>`;
 };
 
-formEl.addEventListener('submit', submitHandler)
+formEl.addEventListener('submit', submitHandler);
+
+
